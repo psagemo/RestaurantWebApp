0b69a54ca88b25b828bd893436ec5b0134bb29e5
Peter Sagemo <psagemo@gmail.com>
2022-11-11 11:55:34 +0100

    Custom menu for user and admin

---
+++ RestaurantWebApp/Models/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using RestaurantWebApp.Areas.Identity.Data;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantWebApp.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [DisplayName("Number of Guests")]
+        [Required(ErrorMessage = "This Field Is Required.")]
+        public int Guests { get; set; }
+
+        [DisplayName("Session")]
+        [Required(ErrorMessage = "This Field Is Required.")]
+        public DateTime DateTime { get; set; }
+
+        [DisplayName("Additional Information")]
+        public string? AdditionalInformation { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
